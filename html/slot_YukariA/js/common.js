509473ecb75efa47ae741352207025e09c169b4f
fund guheheP <fund.blendy@gmail.com>
2018-03-12 20:57:32 +0900

    test

---
+++ html/slot_YukariA/js/common.js
@@ -0,0 +1,699 @@
+//////////////////////////////////////////////////
+////////　共通関数　common.js     /////////////////
+//////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//  バージョン履歴
+/////////////////////////////////////////////////
+    //
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//	定数
+/////////////////////////////////////////////////
+    //
+
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//	共通グローバル変数
+/////////////////////////////////////////////////
+    //リール回転用
+    var reeltop1 = -730;
+    var reeltop2 = -730;
+    var reeltop3 = -730;
+
+    //リール回転制御用
+    var kaiten1;
+    var kaiten2;
+    var kaiten3;
+
+    //キー入力フラグ
+    var keyflag;
+
+    //所持コイン
+    var mycoin = 500;
+
+    //子役フラグ
+    var flag;
+
+    //ゲーム数カウンタ
+    var gamecount = 0;
+    //ボーナス間ゲーム数カウンタ
+    var BBgamecount = 0;
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------//
+//                                                               //
+//---------------------------------------------------------------//
+
+
+/////////////////////////////////////////////////
+// リール回転 1
+/////////////////////////////////////////////////
+function reel1kaiten(){
+
+    var target1 = document.getElementById("reel1"); //対象要素をIDで指定
+    var reel1;
+    
+    reeltop1 += 5
+    if (reeltop1 == 305){
+        reeltop1 = -1180
+    }   
+    reel1 = reeltop1 + "px";
+    target1.style.top = reel1;
+    
+}
+
+/////////////////////////////////////////////////
+// リール回転 2
+/////////////////////////////////////////////////
+function reel2kaiten(){
+
+    var target2 = document.getElementById("reel2"); //対象要素をIDで指定
+    var reel2;
+    
+    reeltop2 += 5
+    if (reeltop2 == 305){
+        reeltop2 = -1180
+    }   
+    reel2 = reeltop2 + "px";
+    target2.style.top = reel2;
+    
+}
+
+/////////////////////////////////////////////////
+// リール回転 3
+/////////////////////////////////////////////////
+function reel3kaiten(){
+
+    var target3 = document.getElementById("reel3"); //対象要素をIDで指定
+    var reel3;
+    
+    reeltop3 += 5
+    if (reeltop3 == 305){
+        reeltop3 = -1180
+    }   
+    reel3 = reeltop3 + "px";
+    target3.style.top = reel3;
+    
+}
+
+
+
+
+/////////////////////////////////////////////////
+// キー入力
+/////////////////////////////////////////////////
+
+function keyinput(){
+
+    if (event.keyCode == 38){  //「↑」が押されたか確認
+        setTimeout(function(){
+            flashreset();
+            if (keyflag == 0){
+                switch(potmode){
+                    case 0: Nomalpot(); break;
+                    case 1: Bonuskakutei(); break;
+                    case 2: Bonusmode(); break;
+                }
+                kaiten1 = setInterval (reel1kaiten,1);
+                kaiten2 = setInterval (reel2kaiten,1);
+                kaiten3 = setInterval (reel3kaiten,1);
+                keyflag = 1;
+            }
+        }, 900);
+    }
+
+    if (event.keyCode == 37){  //「←」が押されたか確認
+        if (keyflag == 1){
+            setTimeout(function(){
+                flashreset();
+                switch (flag){
+                    case "ハズレ": reel1stop(1); break;
+                    case "リプレイ": reel1stop(1); break;
+                    case "ベル": reel1stop(1); break;
+                    case "BBベル": reel1stop(1); break;
+                    case "スイカ": reel1stop(2); break;
+                    case "チェリー": reel1stop(3); break;
+                    case "強チェリー": reel1stop(3); break;
+                    case "強チャンス": reel1stop(2); break;
+                    case "確定チェリー": reel1stop(4); break;
+                    case "BB": reel1stop(5); break;
+                    case "RB": reel1stop(5); break;
+                    default: reel1stop(0);
+                }
+                keyflag = 2
+            }, 150);
+        }
+        
+    }
+
+    if (event.keyCode == 40){  //「↓」が押されたか確認
+        if (keyflag == 2){
+            flashreset();
+            switch (flag){
+                case "ハズレ": reel2stop(1); break;
+                case "リプレイ": reel2stop(1); break;
+                case "ベル": reel2stop(2); break;
+                case "BBベル": reel2stop(2); break;
+                case "スイカ": reel2stop(3); break;
+                case "チェリー": reel2stop(4); break;
+                case "強チェリー": reel2stop(5); break;
+                case "強チャンス": reel2stop(3); break;
+                case "確定チェリー": reel2stop(4); break;
+                case "BB": reel2stop(6); break;
+                case "RB": reel2stop(6); break;
+                default: reel2stop(0);
+            }
+            keyflag = 3
+        }
+    }
+
+    if (event.keyCode == 39){  //「→」が押されたか確認
+        if (keyflag == 3){
+            flashreset();
+            switch (flag){
+                case "ハズレ": reel3stop(1); break;
+                case "リプレイ": reel3stop(2); break;
+                case "ベル": reel3stop(3); break;
+                case "BBベル": reel3stop(3); break;
+                case "スイカ": reel3stop(4); break;
+                case "チェリー": reel3stop(3); break;
+                case "強チェリー": reel3stop(4); break;
+                case "強チャンス": reel3stop(5); break;
+                case "確定チェリー": reel3stop(4); break;
+                case "BB": reel3stop(6); break;
+                case "RB": reel3stop(7); break;
+                default: reel3stop(0);
+            }
+            reel3stop();
+            payout1();
+            keyflag = 0
+        }
+    }
+
+}
+
+/////////////////////////////////////////////////
+// 停止位置調整関数
+/////////////////////////////////////////////////
+function reeloffset(i){
+    var r;
+
+
+    if (i <= -1100 && i >= -1180 ){
+        r = -1105;
+    } 
+    if (i <= -1025 && i >= -1095 ){
+        r = -1031;
+    } 
+    if (i <= -950 && i >= -1020 ){
+        r = -957;
+    } 
+    if (i <= -875 && i >= -945 ){
+        r = -883;
+    } 
+    if (i <= -800 && i >= -870 ){
+        r = -809;
+    } 
+    if (i <= -730 && i >= -800 ){
+        r = -735;
+    } 
+    if (i <= -655 && i >= -725 ){
+        r = -661;
+    }
+    if (i <= -580 && i >= -650 ){
+        r = -587;
+    }
+    if (i <= -505 && i >= -575 ){
+        r = -513;
+    }
+    if (i <= -430 && i >= -500 ){
+        r = -439;
+    }
+    if (i <= -360 && i >= -425 ){ //1リール下BAR　基準位置
+        r = -365;
+    }
+    if (i <= -285 && i >= -355 ){
+        r = -291;
+    }
+    if (i <= -210 && i >= -280 ){
+        r = -217;
+    }
+    if (i <= -135 && i >= -205 ){
+        r = -143;
+    }
+    if (i <= -60 && i >= -130 ){
+        r = -69;
+    }
+    if (i <= 15 && i >= -55 ){
+        r = 5;
+    }
+    if (i <= 90 && i >= 20 ){
+        r = 79;
+    }
+    if (i <= 160 && i >= 95 ){
+        r = 153;
+    }
+    if (i <= 235 && i >= 165 ){
+        r = 227;
+    }
+    if (i <= 300 && i >= 240 ){
+        r = 301;
+    }
+    return r;
+}
+
+
+/////////////////////////////////////////////////
+// リールストップ 1
+/////////////////////////////////////////////////
+function reel1stop(x){
+    switch(x){
+        case 0: break;
+        case 1: reeltop1 = -365; break;
+        case 2: reeltop1 = -290; break;
+        case 3: reeltop1 = -440; break;
+        case 4: reeltop1 = -515; break;
+        case 5: reeltop1 = 235; break;
+    }
+    var i = reeloffset(reeltop1);
+    clearInterval(kaiten1);
+
+    var target1 = document.getElementById("reel1"); //対象要素をIDで指定
+    var reel1;
+    reel1 = i + "px";
+    target1.style.top = reel1;
+}
+
+/////////////////////////////////////////////////
+// リールストップ 2
+/////////////////////////////////////////////////
+function reel2stop(x){
+    switch(x){
+        case 0: break;
+        case 1: reeltop2 = -515; break;
+        case 2: reeltop2 = -135; break;
+        case 3: reeltop2 = -580; break;
+        case 4: reeltop2 = -515; break;
+        case 5: reeltop2 = -365; break;
+        case 6: reeltop2 = 300; break;
+    }
+    var i = reeloffset(reeltop2);
+    clearInterval(kaiten2);
+
+    var target2 = document.getElementById("reel2"); //対象要素をIDで指定
+    var reel2;
+    reel2 = i + "px";
+    target2.style.top = reel2;
+}
+
+/////////////////////////////////////////////////
+// リールストップ 3
+/////////////////////////////////////////////////
+function reel3stop(x){
+    switch(x){
+        case 0: break;
+        case 1: reeltop3 = -440; break;
+        case 2: reeltop3 = -655; break;
+        case 3: reeltop3 = -580; break;
+        case 4: reeltop3 = -210; break;
+        case 5: reeltop3 = -365; break;
+        case 6: reeltop3 = 300; break;
+        case 7: reeltop3 = 155; break;
+    }
+    var i = reeloffset(reeltop3);
+    clearInterval(kaiten3);
+
+    var target3 = document.getElementById("reel3"); //対象要素をIDで指定
+    var reel3;
+    reel3 = i + "px";
+    target3.style.top = reel3;
+}
+
+////////////////////////////////////////////////
+//　払い出し処理
+////////////////////////////////////////////////
+function payout1(){
+
+    var payout;
+    switch (flag){
+        case "ハズレ": payout = NON; break;
+        case "リプレイ": payout = RP; flash1(); break;
+        case "ベル": payout = BELL; flash2(); break;
+        case "BBベル": payout = BELL2; flash2(); break;
+        case "スイカ": payout = SUIKA; flash4(); break;
+        case "チェリー": payout = CHERRY; flash3(); break;
+        case "強チェリー": payout = CHERRY; flash5(); break;
+        case "強チャンス": payout = SUIKA; flash6(); break;
+        case "確定チェリー": payout = CHERRY; flash7(); break;
+        case "BB": payout = RP; Bgame = 20; potmode = 2; flash7(); break;
+        case "RB": payout = RP; Bgame = 8; potmode = 2; flash7(); break;
+        default: payout = NON; break;
+    }
+
+    mycoin += payout;
+
+    document.getElementById("coin").innerHTML=mycoin;
+    document.getElementById("pay").innerHTML=payout;
+
+}
+
+////////////////////////////////////////////////
+//　リールフラッシュ演出
+////////////////////////////////////////////////
+
+function flash1(){ //中段3回フラッシュ
+    
+    var target1 = document.getElementById("f1")
+
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 50);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 100);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 200);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 250);
+
+}
+
+function flash2(){ //上段3回フラッシュ
+    
+    var target1 = document.getElementById("f2")
+
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 50);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 100);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 200);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 250);
+
+}
+
+function flash3(){ //下段フラッシュ
+    
+    var target1 = document.getElementById("f3")
+    var target2 = document.getElementById("f4")
+    var target3 = document.getElementById("f5")
+    var target4 = document.getElementById("f6")
+
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 100);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 150);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 250);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 250);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 400);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 450);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 500);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 550);
+}
+
+function flash4(){ //中段フラッシュ
+    
+    var target1 = document.getElementById("f1")
+    var target2 = document.getElementById("f4")
+    var target3 = document.getElementById("f5")
+    var target4 = document.getElementById("f6")
+
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 100);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 150);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 250);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 250);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 400);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 450);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 500);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 550);
+}
+
+function flash5(){ //下段特殊フラッシュ
+    
+    var target1 = document.getElementById("f3")
+    var target2 = document.getElementById("f4")
+    var target3 = document.getElementById("f5")
+    var target4 = document.getElementById("f6")
+
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 50);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 400);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 450);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 500);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 550);
+}
+
+function flash6(){ //上段特殊フラッシュ
+    
+    var target1 = document.getElementById("f2")
+    var target2 = document.getElementById("f4")
+    var target3 = document.getElementById("f5")
+    var target4 = document.getElementById("f6")
+
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 50);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 400);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 450);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 500);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 550);
+}
+
+function flash7(){ //中段特殊フラッシュ
+    
+    var target1 = document.getElementById("f1")
+    var target2 = document.getElementById("f4")
+    var target3 = document.getElementById("f5")
+    var target4 = document.getElementById("f6")
+
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 50);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 100);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 150);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 1;
+    }, 200);
+    setTimeout(function(){
+        target2.style.opacity = 0;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 1;
+    }, 250);
+    setTimeout(function(){
+        target3.style.opacity = 0;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 1;
+    }, 300);
+    setTimeout(function(){
+        target4.style.opacity = 0;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 350);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 400);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 450);
+    setTimeout(function(){
+        target1.style.opacity = 0;
+    }, 500);
+    setTimeout(function(){
+        target1.style.opacity = 1;
+    }, 550);
+}
+
+
+
+
+
+
+
+//////////////////////////////////
+// フラッシュ初期化
+//////////////////////////////////
+function flashreset(){ //フラッシュリセット
+    
+    document.getElementById("f1").style.opacity = 0;
+    document.getElementById("f2").style.opacity = 0;
+    document.getElementById("f3").style.opacity = 0;
+    document.getElementById("f4").style.opacity = 0;
+    document.getElementById("f5").style.opacity = 0;
+    document.getElementById("f6").style.opacity = 0;
+}
---
+++ html/slot_YukariA/js/yukariA.js
@@ -0,0 +1,213 @@
+//////////////////////////////////////////////////
+////////　本体処理　yukariA.js      ////////////////
+//////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//  バージョン履歴
+/////////////////////////////////////////////////
+    //
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//	定数
+/////////////////////////////////////////////////
+    //消費枚数
+    BET = 3;
+
+    //子役払い出し枚数
+    NON = 0;
+    RP = 3;
+    BELL = 8;
+    SUIKA = 3;
+    CHERRY = 3;
+    BELL2 = 15; //ボーナス中ベル
+
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+//	グローバル変数
+/////////////////////////////////////////////////
+    //モード関連
+    var potmode = 0; //抽選モード
+
+    //ゲーム数関連
+    var Bgame = 0; //ボーナスゲーム　残りゲーム数
+    var Bget = 0; //ボーナス獲得枚数
+
+
+//---------------------------------------------//
+/////////////////////////////////////////////////
+
+
+//---------------------------------------------------------------//
+//                                                               //
+//---------------------------------------------------------------//
+
+/////////////////////////////
+//　通常時抽選
+////////////////////////////
+
+function Nomalpot(){
+
+    var a = Math.floor( Math.random() * 1000 ) ;
+    
+    
+    mycoin -= BET;
+    payout = 0;
+    gamecount += 1;
+    BBgamecount += 1;
+
+
+    document.getElementById("coin").innerHTML=mycoin;
+    document.getElementById("pay").innerHTML=payout;
+    document.getElementById("gamecountA").innerHTML=gamecount;
+    document.getElementById("gamecountB").innerHTML=BBgamecount;
+
+    if (a >= 0 && a <=700){
+        document.getElementById("debug").innerHTML="ハズレ";
+        flag = "ハズレ";
+    }
+    if (a >= 701 && a <=850){
+        document.getElementById("debug").innerHTML="リプレイ";
+        flag = "リプレイ";
+    }
+    if (a >= 851 && a <=950){
+        document.getElementById("debug").innerHTML="ベル";
+        flag = "ベル";
+    }
+    if (a >= 951 && a <=970){
+        document.getElementById("debug").innerHTML="チェリー";
+        flag = "チェリー";
+        Bonuspot(10);
+    }
+    if (a >= 971 && a <=990){
+        document.getElementById("debug").innerHTML="スイカ";
+        flag = "スイカ";
+        Bonuspot(10);
+    }
+    if (a >= 991 && a <=995){
+        document.getElementById("debug").innerHTML="強チェリー";
+        flag = "強チェリー";
+        Bonuspot(33);
+    }
+    if (a >= 995 && a <=999){
+        document.getElementById("debug").innerHTML="強チャンス";
+        flag = "強チャンス";
+        Bonuspot(50);
+    }
+    if (a == 1000){
+        document.getElementById("debug").innerHTML="確定チェリー";
+        flag = "確定チェリー";
+        Bonuspot(100);
+    }
+}
+
+/////////////////////////////
+//　ボーナス確定時抽選
+////////////////////////////
+
+function Bonuskakutei(){
+
+    var a = Math.floor( Math.random() * 1000 ) ;
+    
+    mycoin -= BET;
+    payout = 0;
+    gamecount += 1;
+    BBgamecount += 1;
+
+    document.getElementById("coin").innerHTML=mycoin;
+    document.getElementById("pay").innerHTML=payout;
+    document.getElementById("gamecountA").innerHTML=gamecount;
+    document.getElementById("gamecountB").innerHTML=BBgamecount;
+
+
+    if (a >= 0 && a <=250){
+        document.getElementById("debug").innerHTML="RB";
+        flag = "RB";
+    }
+    if (a >= 251 && a <=500){
+        document.getElementById("debug").innerHTML="BB";
+        flag = "BB";
+    }
+    if (a >= 501 && a <=800){
+        document.getElementById("debug").innerHTML="リプレイ";
+        flag = "リプレイ";
+    }
+    if (a >= 801 && a <=950){
+        document.getElementById("debug").innerHTML="ベル";
+        flag = "ベル";
+    }
+    if (a >= 951 && a <=970){
+        document.getElementById("debug").innerHTML="チェリー";
+        flag = "チェリー";
+        Bonuspot(10);
+    }
+    if (a >= 971 && a <=990){
+        document.getElementById("debug").innerHTML="スイカ";
+        flag = "スイカ";
+        Bonuspot(10);
+    }
+    if (a >= 991 && a <=995){
+        document.getElementById("debug").innerHTML="強チェリー";
+        flag = "強チェリー";
+        Bonuspot(33);
+    }
+    if (a >= 995 && a <=999){
+        document.getElementById("debug").innerHTML="強チャンス";
+        flag = "強チャンス";
+        Bonuspot(50);
+    }
+    if (a == 1000){
+        document.getElementById("debug").innerHTML="確定チェリー";
+        flag = "確定チェリー";
+        Bonuspot(100);
+    }
+}
+
+
+/////////////////////////////
+//　ボーナス中　子役抽選
+////////////////////////////
+
+function Bonusmode(){
+
+    var a = Math.floor( Math.random() * 1000 ) ;
+    
+    mycoin -= BET;
+    payout = 0;
+    
+
+    document.getElementById("coin").innerHTML=mycoin;
+    document.getElementById("pay").innerHTML=payout;
+
+    if (a >= 0 && a <=1000){
+        document.getElementById("debug").innerHTML="BBベル";
+        flag = "BBベル";
+    }
+
+    Bgame -= 1;
+
+    if (Bgame == 0){
+        potmode = 0;
+        BBgamecount =0;
+    }
+}
+
+////////////////////////////////////////////////////////////
+//  ボーナス抽選
+////////////////////////////////////////////////////////////
+function Bonuspot(i){
+
+    var a = Math.floor( Math.random() * 100 ) ;
+    if (a <= i){
+        potmode = 1;
+    }
+    else{
+        potmode = 0;
+    }
+
+}
